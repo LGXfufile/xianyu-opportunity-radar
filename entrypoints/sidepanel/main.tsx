f5c191d9c6691f7c0b15b4417994d48b216c0c0c
LGXfufile <401040990@qq.com>
2026-08-01 22:30:40 +0800

    feat: enable Vercel Web Analytics

--- entrypoints/sidepanel/main.tsx
+++ entrypoints/sidepanel/main.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import { RadarApp } from '../../src/RadarApp';
 import '../../src/styles.css';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><RadarApp /></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RadarApp />
+    <Analytics />
+  </React.StrictMode>,
+);
